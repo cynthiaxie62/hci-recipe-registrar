c51c2ae033d7226f76c9bc5f837b1f9a1bfa5166
xie.cy <xie.cy@husky.neu.edu>
2021-03-21 17:42:12 -0400

    migrated repo

---
+++ LinkingConfiguration.js
@@ -0,0 +1,13 @@
+import * as Linking from 'expo-linking';
+
+/**
+ * Works in conjunction with App to handle navigation.
+ */
+export default {
+  prefixes: [Linking.makeUrl('/')],
+  config: {
+    screens: {
+        
+    },
+  },
+};
