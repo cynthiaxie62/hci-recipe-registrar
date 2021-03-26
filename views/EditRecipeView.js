d253d6450a1afe29051305662b54091e650fc660
xie.cy <xie.cy@husky.neu.edu>
2021-03-26 06:04:34 -0400

    implemented edit page

--- views/EditRecipeView.js
+++ views/EditRecipeView.js
@@ -1,13 +1,101 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { ScrollView } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+import { IconButton } from 'react-native-paper';
+import { Header, Button } from "react-native-elements";
+import {Collapse,CollapseHeader, CollapseBody, AccordionList} from 'accordion-collapse-react-native';
 
 /**
  * This is a view to edit a recipe
  */
 export default function EditRecipeView(props, {navigation, routes}) {
+    const [recipeName, setRecipeName] = useState("Recipe Name")
+    var recipe = [
+        {
+          sectionName: "Ingredients",
+          sectionBody: ["flour", "egg", "water", "oil", "<3"]
+        }, 
+        {
+          sectionName: "Steps",
+          sectionBody: ["step 1", "step 2", "step 3"]
+        }, 
+        {
+          sectionName: "Kitchen Equipment",
+          sectionBody: ["stand mixer", "oven"]
+        }, 
+        {
+          sectionName: "Additional Notes",
+          sectionBody: ["this bread is cool"]
+        }, 
+        {
+          sectionName: "Images",
+          sectionBody: []
+        }, 
+        {
+          sectionName: "Tags",
+          sectionBody: ["baked goods", "italian"]
+        }];
     return (
-        <View style={styles.container}>
-          <Text>Edit Recipe View</Text>
+      <View style={styles.container}>
+            <View style={styles.headerBar}>
+                <Header 
+                    style={{flexDirection: "row", color: "#E0884A"}}
+                    leftComponent= {<IconButton
+                        icon="arrow-left"
+                        color="white"
+                        size={35}
+                        onPress={() => props.navigation.navigate("viewRecipe")}
+                    />}
+                    containerStyle={{
+                        backgroundColor: "#E0884A",
+                        borderBottomColor: "#E0884A"
+                      }}
+                />
+                <View style={{alignSelf: "center"}}>
+                    <TextInput 
+                        style={styles.title} 
+                        value={recipeName} 
+                        onSubmitEditing={value => setRecipeName(value)} //TODO: fix value setting
+                    />
+                </View>
+            </View>
+            <View style={{height: "70%"}}>
+                <ScrollView>
+                    {recipe.map(section => (
+                        <Collapse 
+                            style={styles.accordion}
+                            isCollapsed={true}
+                            key={section.sectionName}>
+                            <CollapseHeader>
+                                <View>
+                                    <Text style={styles.accordionHeader}>{section.sectionName}</Text>
+                                </View>
+                            </CollapseHeader>
+                            <CollapseBody>
+                                {section.sectionBody.map(text => (
+                                    <View key={text} style={{flexDirection: "row", left: "6%"}}>
+                                        <Text style={styles.accordionBody}>&#160;&#8226;&#160;</Text>
+                                        <TextInput value={text} style={styles.accordionBody}/>
+                                    </View>
+                                    
+                                ))}
+                            </CollapseBody>
+                        </Collapse>
+                    ))}
+                    
+                </ScrollView>
+            </View>
+            <View style={styles.footerBar}>
+                <Header
+                    leftComponent={<Button title="Cancel" color="#C32747" />}
+                    rightComponent={<Button title="Save" color="green"/>}
+                    containerStyle={{
+                        backgroundColor: "#F3DFD6",
+                        borderBottomColor: "#F3DFD6"
+                      }}
+                />
+            </View>
       </View>
     );
 }
@@ -15,9 +103,48 @@ export default function EditRecipeView(props, {navigation, routes}) {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      backgroundColor: "#F3DFD6",
+      fontFamily: "PT Serif Caption"
     },
+    headerBar: {
+      backgroundColor: "#E0884A",
+      flexDirection: "column",
+      height: "22%",
+      bottom: "2%"
+    },
+    title: {
+      fontSize: 35,
+      color: "white",
+      borderColor: "black",
+      borderWidth: 1,
+      padding: 5,
+      bottom: "5%"
+    },
+    actionText: {
+      fontSize: 25,
+      color: "white"
+    },
+    accordion: {
+      backgroundColor: "#FAF5F3", 
+      width: "80%", 
+      alignSelf: "center",
+      bottom: "2%"
+    },
+    accordionHeader: {
+      fontSize: 28,
+      top: "5%",
+      left: "3%",
+      bottom: "5%"
+    },
+    accordionBody: {
+      fontSize: 24,
+      left: "6%",
+    },
+    footerBar: {
+      height: "8%",
+      width: "100%",
+      flexDirection: "row", 
+      alignContent: "space-around"
+    }
   });
   
--- views/ViewRecipeView.js
+++ views/ViewRecipeView.js
@@ -4,11 +4,38 @@ import { TouchableOpacity } from 'react-native';
 import { Text, View, StyleSheet } from 'react-native';
 import { IconButton } from 'react-native-paper';
 import { Header, Button } from "react-native-elements";
+import {Collapse,CollapseHeader, CollapseBody, AccordionList} from 'accordion-collapse-react-native';
 
 /**
  * This is a view of a specific recipe.
  */
 export default function ViewRecipeView(props, {navigation, routes}) {
+    var recipe = [
+        {
+          sectionName: "Ingredients",
+          sectionBody: ["flour", "egg", "water", "oil", "<3"]
+        }, 
+        {
+          sectionName: "Steps",
+          sectionBody: ["step 1", "step 2", "step 3"]
+        }, 
+        {
+          sectionName: "Kitchen Equipment",
+          sectionBody: ["stand mixer", "oven"]
+        }, 
+        {
+          sectionName: "Additional Notes",
+          sectionBody: ["this bread is cool"]
+        }, 
+        {
+          sectionName: "Images",
+          sectionBody: []
+        }, 
+        {
+          sectionName: "Tags",
+          sectionBody: ["baked goods", "italian"]
+        }];
+
     return (
         <View style={styles.container}>
             <View style={styles.headerBar}>
@@ -20,7 +47,9 @@ export default function ViewRecipeView(props, {navigation, routes}) {
                         size={35}
                         onPress={() => console.log('back')}
                     />}
-                    rightComponent= {<TouchableOpacity style={{alignSelf: "flex-end", top:"20%", right: "20%"}} onPress={() => console.log("edit")}>
+                    rightComponent= {<TouchableOpacity 
+                        style={{alignSelf: "flex-end", top:"20%", right: "20%"}}
+                        onPress={() => props.navigation.navigate("editRecipe")}>
                         <Text style={styles.actionText}>Edit</Text>
                     </TouchableOpacity>}
                     containerStyle={{
@@ -32,10 +61,28 @@ export default function ViewRecipeView(props, {navigation, routes}) {
                     <Text style={styles.title}>Recipe Name</Text>
                 </View>
             </View>
-            <ScrollView>
-                <Text>View Recipe View</Text>
-            </ScrollView>
-          
+            <View style={{height: "100%"}}>
+                <ScrollView>
+                    {recipe.map(section => (
+                        <Collapse 
+                            style={styles.accordion}
+                            isCollapsed={true}
+                            key={section.sectionName}>
+                            <CollapseHeader>
+                                <View>
+                                    <Text style={styles.accordionHeader}>{section.sectionName}</Text>
+                                </View>
+                            </CollapseHeader>
+                            <CollapseBody>
+                                {section.sectionBody.map(text => (
+                                    <Text key={text} style={styles.accordionBody}>&#8226;&#160;{text}</Text>
+                                ))}
+                            </CollapseBody>
+                        </Collapse>
+                    ))}
+                    
+                </ScrollView>
+            </View>
       </View>
     );
 }
@@ -49,7 +96,8 @@ const styles = StyleSheet.create({
     headerBar: {
       backgroundColor: "#E0884A",
       flexDirection: "column",
-      height: "22%"
+      height: "22%",
+      bottom: "2%"
     },
     title: {
       fontSize: 35,
@@ -58,6 +106,22 @@ const styles = StyleSheet.create({
     actionText: {
       fontSize: 25,
       color: "white"
+    },
+    accordion: {
+      backgroundColor: "#FAF5F3", 
+      width: "80%", 
+      alignSelf: "center",
+      bottom: "2%"
+    },
+    accordionHeader: {
+      fontSize: 28,
+      top: "5%",
+      left: "3%",
+      bottom: "5%"
+    },
+    accordionBody: {
+      fontSize: 24,
+      left: "6%",
     }
   });
   
